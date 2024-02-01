e744e35ed7343048c518ec944618142eb3decf74
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-01 17:58:47 +0100

    first page construction

--- vite-project/src/MainContent/MainContent.jsx
+++ vite-project/src/MainContent/MainContent.jsx
@@ -1,3 +1,5 @@
+import ReactPlayer from 'react-player'
+import firstVideo from '../assets/first-video.mp4'
 import backgroundImage from '../assets/background-logo.png'
 import './MainContent.css'
 
@@ -5,6 +7,11 @@ function MainContent(){
   return(
     <article className="main-page">
       <img src={backgroundImage} className='background-image' alt='Baby feets black and white' />
+      <div className='presentation'>
+        <h1 className='title'>Ostetrica Silvia Buzzoni <br/> Libera Professionisa a Ferrara</h1>
+        <p className='sub-title'>Prendersi cura. Della donna, della madre, dei bimbi</p>
+        <ReactPlayer url={firstVideo} controls={true} className='first-video' />
+      </div>
     </article>
   )
 }
