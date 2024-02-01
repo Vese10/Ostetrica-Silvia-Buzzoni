f74c54ea1e2ad3dd00b0978bb492756619135526
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-01 20:02:26 +0100

    second page style

--- vite-project/src/MainContent/MainContent.jsx
+++ vite-project/src/MainContent/MainContent.jsx
@@ -1,16 +1,26 @@
 import ReactPlayer from 'react-player'
 import firstVideo from '../assets/first-video.mp4'
 import backgroundImage from '../assets/background-logo.png'
+import backgroundImage2 from '../assets/second-page-backgrond-image.jpg'
 import './MainContent.css'
 
 function MainContent(){
   return(
     <article className="main-page">
-      <img src={backgroundImage} className='background-image' alt='Baby feets black and white' />
-      <div className='presentation'>
-        <h1 className='title'>Ostetrica Silvia Buzzoni <br/> Libera Professionisa a Ferrara</h1>
-        <p className='sub-title'>Prendersi cura. Della donna, della madre, dei bimbi</p>
-        <ReactPlayer url={firstVideo} width='300' controls={true} autoplay={true} className='first-video' />
+      <div className='first-page'>
+        <img src={backgroundImage} className='background-image' alt='Baby feets black and white' />
+        <div className='presentation'>
+          <h1 className='title'>Ostetrica Silvia Buzzoni <br/> Libera Professionisa a Ferrara</h1>
+          <p className='sub-title'>Prendersi cura. Della donna, della madre, dei bimbi</p>
+          <ReactPlayer url={firstVideo} width='300' controls={true} autoPlay={true} className='first-video' />
+        </div>
+      </div>
+      <div className='second-page'>
+        <img src={backgroundImage2} className='background-image-2' alt='The midwife into her studio' />
+        <div className='about-me'>
+          <h2 className='about-me-title'>Chi sono?</h2>
+          <p className='about-me-text'>Mi chiamo Silvia Buzzoni e sono nata a Portomaggiore (FE). Mi sono laureata nel 2018 all’università degli studi di Ferrara. Svolgo la mia libera professione nella provincia di Ferrara e dintorni. Con il mio lavoro cerco di fare quello che la società spesso non è pronta a sostenere: prendermi cura di noi donne. Ogni donna, che sia madre, figlia, sorella, nonna, amica ha il diritto ad essere correttamente assistita, informata e formata in ogni suo bisogno ed è quello che provo a fare: stare al vostro fianco.</p>
+        </div>
       </div>
     </article>
   )
