cca0711bd8b2f0598ccd265b7d46f17b831dd65b
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-01 11:32:27 +0100

    header and footer ok

--- vite-project/src/MainContent/MainContent.jsx
+++ vite-project/src/MainContent/MainContent.jsx
@@ -1,9 +1,10 @@
+import backgroundImage from '../assets/background-logo.png'
 import './MainContent.css'
 
 function MainContent(){
   return(
     <div className="main-page">
-      
+      <img src={backgroundImage} className='background-image' alt='Baby feets black and white' />
     </div>
   )
 }
