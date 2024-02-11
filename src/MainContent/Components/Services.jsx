c44972138b4c90c939f7fdadc87e7d962b76de80
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-11 10:56:33 +0100

    onclick details

--- src/MainContent/Components/Services.jsx
+++ src/MainContent/Components/Services.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import './Services.css'
 
 function Services({children}){
+  const [showDiv, setShowDiv] = useState(false)
+  const handleShowDiv = () => {
+    setShowDiv(!showDiv);
+  };
+
   return(
     <div className='servizi-container'>
-      <p className='servizi'>{children}</p>
+      <p className='servizi' onClick={handleShowDiv}>{children}</p>
+      {showDiv && (
+        <div className='div-show'>
+          <p className='p-show'>Ciao</p>
+        </div>
+      )}
     </div>
   )
 }
